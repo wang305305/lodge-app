fcbaa93a8b094dd1bb9ffc7f4d465b9cc1d870a7
wang305305 <wang305305@gmail.com>
2022-04-26 14:35:18 -0400

    show user current location

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import '@angular/google-maps';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-map',
@@ -15,11 +16,35 @@ export class MapComponent implements OnInit {
   }
 
   initMap() {
-    console.log("initMap")
-    this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8,
-    });
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(res => {
+        console.log(res)
+        let userLocation = { lat: res.coords.latitude, lng: res.coords.longitude };
+        const map = new google.maps.Map(
+          document.getElementById("map") as HTMLElement,
+          {
+            zoom: 8,
+            center: userLocation,
+          }
+        );
+      
+        new google.maps.Marker({
+          position: userLocation,
+          map,
+          title: "Hello World!",
+        });43.651070
+      });
+    } else {
+      Swal.fire("Unable to get geolocation")
+    }
+    const map = new google.maps.Map(
+      document.getElementById("map") as HTMLElement,
+      {
+        zoom: 6,
+        center: { lat: 43.651070, lng: -79.347015 },
+      }
+    );
+    
   }
   
   //window.initMap = this.initMap;
